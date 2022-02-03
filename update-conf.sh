39ab6f7f16c9dec7f4510af215d5a349987d857c
C. Tarbide <ctarbide@tuta.io>
2022-02-03 23:06:17 +0000

    add ability to specify a custom m4 processor and prioritize om4 and qm4, fallback to m4
    - om4: https://github.com/ibara/m4
    - qm4: https://github.com/ctarbide/qm4

--- update-conf.sh
+++ update-conf.sh
@@ -9,6 +9,22 @@ thisdir=${thispath%/*}
 
 cd "${thisdir}"
 
+M4=${M4:-}
+
+if [ x"${M4}" = x ]; then
+    if type om4 >/dev/null 2>&1; then
+        # https://github.com/ibara/m4
+        M4=om4
+    elif type qm4 >/dev/null 2>&1; then
+        # https://github.com/ctarbide/qm4
+        M4=qm4
+    elif type m4 >/dev/null 2>&1; then
+        M4=m4
+    else
+        die 1 "error: no m4 program detected"
+    fi
+fi
+
 current_dir=
 if [ -f CURRENT_DIR ]; then
     . ./CURRENT_DIR
@@ -29,7 +45,7 @@ m4_define(\`HOME',\`${HOME}')m4_dnl
 m4_define(\`PWD',\`${thisdir}')m4_dnl
 m4_define(\`LISTEN',\`${LISTEN}')m4_dnl
 EOF
-    m4 -P cache-update-conf.m4 nginx.conf.m4 > nginx.conf.tmp
+    ${M4} -P cache-update-conf.m4 nginx.conf.m4 > nginx.conf.tmp
     mv -f nginx.conf.tmp nginx.conf
     chmod a-w nginx.conf
 fi
